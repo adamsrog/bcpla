4d3bdaf6ef9dffe01c1b1cfa3a1a35788328f73f
Roger Adams <adamsrog04@gmail.com>
2015-01-12 15:49:19 -0800

    lots of design work

--- app/router.js
+++ app/router.js
@@ -1,13 +1,15 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.route('occupation');
-  this.route('crosswalk');
+  this.resource("pla", function() {
+  	this.route("occupation");
+  	this.route("crosswalk");
+  });
 });
 
 export default Router;
