61b8f122422ebab5d0fb28e315aec081d2424a0f
Roger Adams <adamsrog04@gmail.com>
2015-02-25 20:20:51 -0800

    refactored handlebars helper into a computed property on the occupation-row component

--- app/components/occupation-row.js
+++ app/components/occupation-row.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	matchBadgeClass: Ember.computed('occupation.match', function() {
+		var match = this.get('occupation.match');
+		if (match === "M") {
+			this.set('matchText', 'Medium');
+			return 'is-medium';
+		} else if (match === "H") {
+			this.set('matchText', 'High');
+			return 'is-high';
+		}
+		return '';
+	})
 });
