66c39bb76b0aa8c1eb6a769250ee32da9ed7f101
Roger Adams <adamsrog04@gmail.com>
2015-10-28 12:06:18 -0700

    fix clear categories bug

--- app/routes/pla/crosswalk.js
+++ app/routes/pla/crosswalk.js
@@ -10,6 +10,38 @@ export default Ember.Route.extend({
 			this.controllerFor('pla/crosswalk').set('filterQuery', '');
 		},
 		clearCategories: function() {
+			this.controllerFor('pla/crosswalk').setProperties({
+				selectedBusinessAnalytics: false,
+				selectedBusinessIntelligence: false,
+				selectedComputerNetwork: false,
+				selectedDataAnalysis: false,
+				selectedDatabase: false,
+				selectedDatabaseAdministration: false,
+				selectedElectronicHealthRecords: false,
+				selectedHealthCare: false,
+				selectedHealthCareAssistant: false,
+				selectedHealthCareDatabase: false,
+				selectedHealthCareSpecialist: false,
+				selectedHealthInformatics: false,
+				selectedHealthInformationTechnology: false,
+				selectedHealthTechnician: false,
+				selectedHealthcareAnalyst: false,
+				selectedHealthcareTechnology: false,
+				selectedHelpDesk: false,
+				selectedInformationTechnology: false,
+				selectedMedicalAssistant: false,
+				selectedMedicalCharting: false,
+				selectedMedicalRecords: false,
+				selectedNetworkAdministrator: false,
+				selectedNetworkAnalyst: false,
+				selectedNetworkSecurity: false,
+				selectedNetworkSystems: false,
+				selectedPatientCare: false,
+				selectedPharmacy: false,
+				selectedPharmacySpecialist: false,
+				selectedPharmacyTechnician: false,
+				selectedSoftwareDevelopment: false
+			});
 			this.controllerFor('pla/crosswalk').set('sortCategories', []);
 		},
 		toggleFilters: function() {
